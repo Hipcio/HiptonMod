512110382eb9352ebf8af95189cde85efa1e1c33
Filip Celejewski <FilipCelejewski@wp.pl>
2018-06-02 20:19:46 +0200

    hipton stone disappears when clicked with diamond axe

--- src/main/java/pl/hipcio/hiptonmod/HiptonEggBlock.java
+++ src/main/java/pl/hipcio/hiptonmod/HiptonEggBlock.java
@@ -3,20 +3,20 @@ package pl.hipcio.hiptonmod;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class HiptonEggBlock extends Block {
 
+    public HiptonEggBlock() {
+        super(Material.ROCK);
 
-public HiptonEggBlock() {
-    super(Material.ROCK);
-   
-    setRegistryName("hiptonEggBlock");
-    setUnlocalizedName("hiptonEggBlock");
-    setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-    setHardness(10.5f);
-    
+        setRegistryName("hiptonEggBlock");
+        setUnlocalizedName("hiptonEggBlock");
+        setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        setHardness(10.5f);
+
+    }
+
 }
-}
-
-
-
--- src/main/java/pl/hipcio/hiptonmod/HiptonStoneBlock.java
+++ src/main/java/pl/hipcio/hiptonmod/HiptonStoneBlock.java
@@ -3,21 +3,29 @@ package pl.hipcio.hiptonmod;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
-
-
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class HiptonStoneBlock extends Block {
 
     public HiptonStoneBlock() {
         super(Material.ROCK);
-       
+
         setRegistryName("hiptonStoneBlock");
         setUnlocalizedName("hiptonStoneBlock");
         setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
         setHardness(15.5f);
-        
-      
-      }
+
     }
 
-
+    @Override
+    public void onBlockClicked(World world, BlockPos position, EntityPlayer player) {
+        super.onBlockClicked(world, position, player);
+        System.out.println("Jestem tutaj! Kliknęty! " + player.getHeldItemMainhand().getItem().getUnlocalizedName());
+        
+        if (player.getHeldItemMainhand().getItem().getUnlocalizedName().equals("item.hatchetDiamond")) {
+            world.setBlockToAir(position);
+        }
+    }
+}
